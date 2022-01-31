98d5bd3c9cacdff2ace4a4e8763eb84368e53221
Sunbound <jhorger95@gmail.com>
2022-01-30 21:33:44 -0600

    added changing colors based on time of day

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,44 @@
-// Current Date
+var saveBtn = document.getElementById("save");
+
+// Current Date/Time
 var today = new Date();
 document.getElementById("currentDay").innerHTML = today;
 
+// Current hour of the day
+var timeSlots = document.getElementsByClassName("textarea");
+var time = parseInt(moment().format("H"));
+console.log(time);
 
+// If/Then statements for coloring time slots
+Array.from(timeSlots).forEach(textarea => {
+    var slotIdString = textarea.id, textareaHour;
+    if (slotIdString) {
+        textareaHour = parseInt(slotIdString);
+    }
+
+    // Compare row hour to current time and change color
+    if (textareaHour) {
+        if (time === textareaHour) {
+            setColor(textarea, "red");
+        }
+        else if (time < textareaHour) {
+            setColor(textarea, "green");
+        }
+        else if (time > textareaHour) {
+            setColor(textarea, "lightgrey");
+        }
+        else {
+            setColor(textarea, "white");
+        }
+    }
+});
+
+function setColor(textarea, color) {
+    textarea.style.backgroundColor = color;
+};
+
+function saveButton() {
+    
+}
+
+saveBtn.onclick = saveButton;
