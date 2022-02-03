20ee1c1bc5e62239f5fdd767f898fad5ae2a5091
Sunbound <jhorger95@gmail.com>
2022-02-03 17:37:05 -0600

    local storage working now

--- Develop/script.js
+++ Develop/script.js
@@ -64,8 +64,58 @@ buttons.forEach(button => {
 
         // Load the saved notes into the correct time slot
         var savedNote = localStorage.getItem("textarea" + currentId);
+        localStorage.getItem("textarea" + currentId);
         savedNote = JSON.parse(savedNote);
-        console.log(savedNote);
-        textarea = savedNote.value;
+        // console.log(currentId);
+        // var textStr = currentId.toString();
+        // var textArea = document.getElementById(textStr);
+        // textArea.textContent = savedNote;
+    
     });
 });
+
+// Section for pulling the text content from local storage.
+var hour9 = document.getElementById("9");
+localStorage.getItem("textarea9");
+var savedHour = JSON.parse(localStorage.getItem("textarea9"));
+hour9.textContent = savedHour;
+
+var hour10 = document.getElementById("10");
+localStorage.getItem("textarea10");
+var savedHour = JSON.parse(localStorage.getItem("textarea10"));
+hour10.textContent = savedHour;
+
+var hour11 = document.getElementById("11");
+localStorage.getItem("textarea11");
+var savedHour = JSON.parse(localStorage.getItem("textarea11"));
+hour11.textContent = savedHour;
+
+var hour12 = document.getElementById("12");
+localStorage.getItem("textarea12");
+var savedHour = JSON.parse(localStorage.getItem("textarea12"));
+hour12.textContent = savedHour;
+
+var hour13 = document.getElementById("13");
+localStorage.getItem("textarea13");
+var savedHour = JSON.parse(localStorage.getItem("textarea13"));
+hour13.textContent = savedHour;
+
+var hour14 = document.getElementById("14");
+localStorage.getItem("textarea14");
+var savedHour = JSON.parse(localStorage.getItem("textarea14"));
+hour14.textContent = savedHour;
+
+var hour15 = document.getElementById("15");
+localStorage.getItem("textarea15");
+var savedHour = JSON.parse(localStorage.getItem("textarea15"));
+hour15.textContent = savedHour;
+
+var hour16 = document.getElementById("16");
+localStorage.getItem("textarea16");
+var savedHour = JSON.parse(localStorage.getItem("textarea16"));
+hour16.textContent = savedHour;
+
+var hour17 = document.getElementById("17");
+localStorage.getItem("textarea17");
+var savedHour = JSON.parse(localStorage.getItem("textarea17"));
+hour17.textContent = savedHour;
